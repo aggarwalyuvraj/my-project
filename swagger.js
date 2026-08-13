26c68aef6dd867d54d8ff96f777fc9f2d088ade7
aggarwalyuvraj <aggarwalyuvraj87@gmail.com>
2026-08-13 19:03:25 +0530

    Add files via upload

---
+++ swagger.js
@@ -0,0 +1,25 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+
+        info: {
+            title: "Student Management API",
+            version: "1.0.0",
+            description: "API documentation for Student Management System"
+        },
+
+        servers: [
+            {
+                url: "http://localhost:3001"
+            }
+        ]
+    },
+
+    apis: ["./router/*.js"]
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
