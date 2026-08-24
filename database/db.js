c257e4f69ef97e9eb44d623cd041f8a0f0ed5b75
aggarwalyuvraj <aggarwalyuvraj87@gmail.com>
2026-08-24 17:37:39 +0530

    Update MongoDB connection string and database name

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
 
-const url = "mongodb://127.0.0.1:27017";
+// const url = "mongodb://127.0.0.1:27017";
+const url="mongodb+srv://aggarwalyuvraj87_db_user:7BCnwr1Us3BGShXt@cluster0.vmxrwac.mongodb.net/?appName=Cluster0"
 const client = new MongoClient(url);
 
 let db;
@@ -12,7 +13,7 @@ async function connectDB() {
 
         console.log("MongoDB Connected Successfully");
 
-        db = client.db("local");   
+        db = client.db("sample_mflix");   
 
         return db;
 
